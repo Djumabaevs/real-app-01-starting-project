55e2f1f1792a391e0f3f344f474a53e40a6eb7bf
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-22 23:39:06 +0600

    start game screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number"/>
+      <StartGameScreen/>
     </View>
   );
 }
