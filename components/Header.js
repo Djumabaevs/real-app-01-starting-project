d79da2989675110b3a211cc56806182dcd0d202a
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-22 19:30:52 +0600

    styled header

--- components/Header.js
+++ components/Header.js
@@ -11,10 +11,16 @@ const Header = props => {
 
 const styles = StyleSheet.create({
     header: {
-        width: '100%'
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: '#f7287b',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     headerTitle: {
-        
+        color: 'black',
+        fontSize: 18
     }
 })
 
