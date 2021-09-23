f35f53f2b2224faefb62c9a1efad2a1c098efe8a
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-23 10:42:14 +0600

    input js

---
+++ components/Input.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+const Input = props => {
+  return <TextInput {...props} style={{ ...styles.input, ...props.style }} />;
+};
