49f6c478dbe3f5ff9e8b14b7ad9cfeb8756afe65
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-24 13:18:37 +0600

    custom styles

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -33,4 +33,41 @@ const generateRandomBetween = (min, max, exclude) => {
     </View>
   );
 
+
+
+
+const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      padding: 10,
+      alignItems: 'center'
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      marginTop: 20,
+      width: 400,
+      maxWidth: '90%'
+    },
+    listContainer: {
+      flex: 1,
+      width: '60%'
+    },
+    list: {
+      flexGrow: 1,
+      // alignItems: 'center',
+      justifyContent: 'flex-end',
+    },
+    listItem: {
+      borderColor: '#ccc',
+      borderWidth: 1,
+      padding: 15,
+      marginVertical: 10,
+      backgroundColor: 'white',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '100%'
+    }
+  });
   
+  export default GameScreen;
