847f1aebbe0416c716b17dbf942ab7286d94e8d1
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-26 00:07:24 +0600

    game over screen

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import MainButton from '../components/MainButton';
+import Colors from '../constants/colors';
+
