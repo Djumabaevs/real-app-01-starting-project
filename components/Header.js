78a73bda3fb0bfb9da947051dad9f35b1c229951
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-26 00:17:08 +0600

    done

--- components/Header.js
+++ components/Header.js
@@ -1,70 +1,26 @@
-import React from "react";
-import {View, StyleSheet, Text} from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const fetchFonts = () => {
-    return Font.loadAsync({
-      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-    });
-  };
-  
-  export default function App() {
-    const [userNumber, setUserNumber] = useState();
-    const [guessRounds, setGuessRounds] = useState(0);
-    const [dataLoaded, setDataLoaded] = useState(false);
-  
-    if (!dataLoaded) {
-      return (
-        <AppLoading
-          startAsync={fetchFonts}
-          onFinish={() => setDataLoaded(true)}
-          onError={err => console.log(err)}
-        />
-      );
-    }
-  
-    const configureNewGameHandler = () => {
-      setGuessRounds(0);
-      setUserNumber(null);
-    };
-  
-    const startGameHandler = selectedNumber => {
-      setUserNumber(selectedNumber);
-    };
-  
-    const gameOverHandler = numOfRounds => {
-      setGuessRounds(numOfRounds);
-    };
-  
-    let content = <StartGameScreen onStartGame={startGameHandler} />;
-  
-    if (userNumber && guessRounds <= 0) {
-      content = (
-        <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
-      );
-    } else if (guessRounds > 0) {
-      content = (
-        <GameOverScreen
-          roundsNumber={guessRounds}
-          userNumber={userNumber}
-          onRestart={configureNewGameHandler}
-        />
-      );
-    }
+import TitleText from './TitleText';
+import Colors from '../constants/colors';
+
+const Header = props => {
+  return (
+    <View style={styles.header}>
+      <TitleText>{props.title}</TitleText>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    header: {
-        width: '100%',
-        height: 90,
-        paddingTop: 36,
-        backgroundColor: '#f7287b',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    headerTitle: {
-        color: 'black',
-        fontSize: 18
-    }
-})
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: Colors.primary,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default Header;
