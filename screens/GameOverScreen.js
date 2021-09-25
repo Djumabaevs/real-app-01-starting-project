765921711a838112612e2a48bb9e3f249ee8e3a7
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-26 00:09:27 +0600

    added final stylization to game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -34,5 +34,37 @@ const GameOverScreen = props => {
       </View>
     );
   };
-
+  const styles = StyleSheet.create({
+    screen: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    imageContainer: {
+      width: 300,
+      height: 300,
+      borderRadius: 150,
+      borderWidth: 3,
+      borderColor: 'black',
+      overflow: 'hidden',
+      marginVertical: 30
+    },
+    image: {
+      width: '100%',
+      height: '100%'
+    },
+    resultContainer: {
+      marginHorizontal: 30,
+      marginVertical: 15
+    },
+    resultText: {
+      textAlign: 'center',
+      fontSize: 20
+    },
+    highlight: {
+      color: Colors.primary,
+      fontFamily: 'open-sans-bold'
+    }
+  });
   
+  export default GameOverScreen;
