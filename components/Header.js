29df6c576ffe86ce9c0c3e62eb998589cc4a701d
BakytDzhumabaev <djumabaevb@gmail.com>
2021-09-26 00:15:32 +0600

    different handlers implemented in app js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,56 @@
 import React from "react";
 import {View, StyleSheet, Text} from 'react-native';
 
-const Header = props => {
-    return(
-        <View style={styles.header}>
-            <Text style={styles.headerTitle}>{props.title}</Text>
-        </View>
-    )
-}
+const fetchFonts = () => {
+    return Font.loadAsync({
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+  };
+  
+  export default function App() {
+    const [userNumber, setUserNumber] = useState();
+    const [guessRounds, setGuessRounds] = useState(0);
+    const [dataLoaded, setDataLoaded] = useState(false);
+  
+    if (!dataLoaded) {
+      return (
+        <AppLoading
+          startAsync={fetchFonts}
+          onFinish={() => setDataLoaded(true)}
+          onError={err => console.log(err)}
+        />
+      );
+    }
+  
+    const configureNewGameHandler = () => {
+      setGuessRounds(0);
+      setUserNumber(null);
+    };
+  
+    const startGameHandler = selectedNumber => {
+      setUserNumber(selectedNumber);
+    };
+  
+    const gameOverHandler = numOfRounds => {
+      setGuessRounds(numOfRounds);
+    };
+  
+    let content = <StartGameScreen onStartGame={startGameHandler} />;
+  
+    if (userNumber && guessRounds <= 0) {
+      content = (
+        <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+      );
+    } else if (guessRounds > 0) {
+      content = (
+        <GameOverScreen
+          roundsNumber={guessRounds}
+          userNumber={userNumber}
+          onRestart={configureNewGameHandler}
+        />
+      );
+    }
 
 const styles = StyleSheet.create({
     header: {
